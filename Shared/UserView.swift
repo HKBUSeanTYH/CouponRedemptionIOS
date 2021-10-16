8447eae032ae56bf7b914e7e8c30af58cebfe167
HKBUSeanTYH <18200265@life.hkbu.edu.hk>
2021-10-16 13:10:59 +0800

    still working on user tab. developed a neat toggled to swap between different items in list

--- Shared/UserView.swift
+++ Shared/UserView.swift
@@ -9,20 +9,40 @@ import SwiftUI
 
 struct UserView: View {
     @State private var username: String = "Placeholder Username"
+    @State private var loggedIn = false
     
     var body: some View {
         NavigationView{
             VStack {
                 HStack {
-                    RemoteImageView(urlString: "https://bulma.io/images/placeholders/128x128.png")
+                    RemoteImageView(urlString: "https://bulma.io/images/placeholders/128x128.png").frame(width: 80.0, height:80).padding(.trailing, 40.0)
                     Text(username)
-                }
-                List (userPageItem.data){ items in
-                    HStack{
-                        Text(items.item)
+                }.padding(.top, 20)
+                if (!loggedIn){
+                    List (userPageItem.data.filter{$0.item == "log in"}){ items in
+                        HStack{
+                            //Text(items.item)
+                            Button(action: {
+                                self.loggedIn.toggle()
+                            }) {
+                                Text(items.item)
+                            }
+                        }
                     }
-                }.navigationBarTitle("User", displayMode: .inline)
-            }
+                }else {
+                    List (userPageItem.data.filter{$0.item != "log in"}){ items in
+                        HStack{
+                            Button(action: {
+                                if (items.item == "log out"){
+                                    self.loggedIn.toggle()
+                                }
+                            }) {
+                                Text(items.item)
+                            }
+                        }
+                    }
+                }
+            }.navigationBarTitle("User Profile", displayMode: .inline)
         }
     }
 }
